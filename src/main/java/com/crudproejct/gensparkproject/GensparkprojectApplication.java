52ccc066b5783bc27e1d1874b5d5df91ef443815
Kunhyuk <klee762@gatech.edu>
2023-05-30 14:01:19 -0400

    backend

---
+++ src/main/java/com/crudproejct/gensparkproject/GensparkprojectApplication.java
@@ -0,0 +1,13 @@
+package com.crudproejct.gensparkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GensparkprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GensparkprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crudproejct/gensparkproject/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.crudproejct.gensparkproject.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String s) {
+        super(s);
+
+    }
+}
